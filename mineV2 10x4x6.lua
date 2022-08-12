31a6767131424a8a863223867f155f8f2fcd3401
EdisPL <111054925+EdisPL@users.noreply.github.com>
2022-08-12 17:28:20 +0300

    Update mineV2 10x4x6.lua

---
+++ mineV2 10x4x6.lua
@@ -0,0 +1,22 @@
+for z=1, 9 do
+    turtle.dig()
+    turtle.forward()
+    turtle.turnRight()
+for y=1, 4 do
+for x=1, 5 do
+    turtle.dig()
+    turtle.forward()
+end
+turtle.turnLeft()
+turtle.turnLeft()
+turtle.digUp()
+turtle.up()
+end
+turtle.turnLeft()
+for y2=1, 4 do
+    turtle.down()
+end
+end
+for z2=1, 9 do
+    turtle.back()
+end
