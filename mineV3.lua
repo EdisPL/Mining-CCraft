5ac005930abc61147c34b738066d0c4883b2c4d3
EdisPL <111054925+EdisPL@users.noreply.github.com>
2022-08-14 23:31:50 +0300

    Update mineV3.lua

---
+++ mineV3.lua
@@ -0,0 +1,48 @@
+turtle.dig()
+turtle.forward()
+for d=1, 5 do
+for a=1, 9 do
+    turtle.dig()
+    turtle.digUp()
+    turtle.forward()
+end
+for b=1, 2 do
+    turtle.turnRight()
+    turtle.digUp()
+    turtle.up()
+end
+for c=1, 9 do
+    turtle.dig()
+    turtle.digUp()
+    turtle.forward()
+end
+turtle.turnRight()
+turtle.down()
+turtle.down()
+turtle.dig()
+turtle.forward()
+turtle.turnRight()
+end
+for a=1, 9 do
+    turtle.dig()
+    turtle.digUp()
+    turtle.forward()
+end
+for b=1, 2 do
+    turtle.turnRight()
+    turtle.digUp()
+    turtle.up()
+end
+for a=1, 9 do
+    turtle.dig()
+    turtle.digUp()
+    turtle.forward()
+end
+turtle.turnLeft()
+turtle.down()
+turtle.down()
+for e=1, 5 do
+turtle.forward()
+end
+turtle.turnRight()
+turtle.back()
